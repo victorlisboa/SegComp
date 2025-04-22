5e53154cd481420a7c78f44d302453da7e274a65
Victor Hugo <victorhugof.lisboa@gmail.com>
2025-04-21 21:00:10 -0300

    suporte a texto com espaço

--- LE01/Ex1/shift_cipher.py
+++ LE01/Ex1/shift_cipher.py
@@ -1,22 +1,24 @@
-import sys
-
 def shift_encrypt(plaintext, k):
     encrypted_text = ''
     for l in plaintext:
-        encrypted_text += chr((ord(l) - 97 + k) % 26 + 97)
+        if l.isalpha():
+            encrypted_text += chr((ord(l) - 97 + k) % 26 + 97)
+        else:
+            encrypted_text += l
     return encrypted_text
 
 def shift_decrypt(ciphertext, k):
     decrypted_text = ''
     for l in ciphertext:
-        decrypted_text += chr((ord(l) - 97 - k) % 26 + 97)
+        if l.isalpha():
+            decrypted_text += chr((ord(l) - 97 - k) % 26 + 97)
+        else:
+            decrypted_text += l
     return decrypted_text
 
 def main():
-    plaintext = sys.argv[1]
-    k = 3
-    if len(sys.argv) > 2:
-        k = int(sys.argv[2])
+    plaintext = input('Texto a ser cifrado: ').lower()
+    k = int(input('Chave k: '))
     ciphertext = shift_encrypt(plaintext, k)
     decrypted_text = shift_decrypt(ciphertext, k)
     print(f'Texto criptografado por deslocamento com o k=3: {ciphertext}')
